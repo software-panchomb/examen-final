00d6f173db340cabbfded8ef986c100a9c4b0687
panchomb <francisco.magot@utec.edu.pe>
2022-07-12 21:31:06 -0500

    remove create_app

--- app.py
+++ app.py
@@ -31,11 +31,5 @@ def load_messages(topic):
 
     return jsonify(topic_messages)
 
-
-def create_app():
-    app = Flask(__name__)
-
-    return app
-
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000, debug = True)
